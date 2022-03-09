6a5552980c1b48ff66618490c4d7f270f669b7ea
Nour Eddin Alsabbagh <nour.d.sabbagh92@gmail.com>
2022-03-09 09:35:04 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// imports
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = 4001;
+
+// Global middleware
+app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('we are connected to the database.');
+  })
+  .catch((error) => {
+    console.log('an error occurred while connecting ot the db', error.message);
+  });
+
+////////////////////////////
+app.listen(process.env.PORT || port, () => {
+  console.log(`connected to ${port}`);
+});
