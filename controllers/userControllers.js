733e6d774b5e2de162da7694149fbff4944a98a3
ramazankarisan <rkarisan25@gmail.com>
2022-03-09 13:30:06 +0100

    create logIn controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,37 @@
 const User = require('../models/userModel');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-exports.register = () => {};
+exports.register = () => { };
 
-exports.logIn = () => {};
+exports.logIn = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    // check if the user exists
+    const foundUser = await User.findOne({ email: email }).lean();
+    if (!foundUser) {
+      return res
+        .status(404)
+        .json({ errMsg: 'User not found, please register!' })
+    }
+    // compare the passwords
+    const passwordIsValid = await bcrypt.compare(password, foundUser.password);
+    if (!passwordIsValid) {
+      return res.status(400).json({ errMsg: 'invalid password!' });
+    }
+
+    // send a token with cookies
+    const payload = { id: foundUser._id, email: foundUser.email };
+    const token = jwt.sign(payload, process.env.COOKIE_SECRET, { expiresIn: "1h" });
+    res
+      .status(200)
+      .cookie("token_cookie", token, { httpOnly: true, secure: false })
+      .json({ msg: "you successfully logged in!" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+
+
+};
--- models/userModel.js
+++ models/userModel.js
@@ -2,10 +2,10 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
-  password = {type: String, required:true}
+  password: { type: String, required: true }
 });
 
-const User = model("User",userSchema)
+const User = model("User", userSchema)
 
 
 
