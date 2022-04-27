b1011ffbb961fd293e06a8f0eee6de24132c2b71
ramazankarisan <rkarisan25@gmail.com>
2022-04-27 16:20:39 +0200

    heroku deploy

--- server.js
+++ server.js
@@ -37,10 +37,14 @@ app.use('/quiz', quizRouter);
 
 app.use(handleAllErrors);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 ///////////////////////
 app.listen(process.env.PORT || port, () => {
