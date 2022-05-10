20001a31d5b03e959780c273701d96e370a7e55f
ramazankarisan <rkarisan25@gmail.com>
2022-05-10 11:47:21 +0200

    add animation to home comp

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { translate, translateHistory } from '../redux/actions/homeActions';
 import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
+import { inputLeftVariants, inputRightVariants, slidDown, slideUpVariants } from '../animation/animationVarients';
+
 
 const Home = () => {
   const [text, setText] = useState('');
@@ -29,6 +32,7 @@ const Home = () => {
   };
   useEffect(() => {
     handleHistory();
+
   }, [translations]);
 
   // translate history words on click
@@ -41,43 +45,49 @@ const Home = () => {
 
   return (
     <>
-      {isLoading ? ("loading") : (<>
+      <div className="homeContainer">
+        <div className="searchAndResults" >
+          <form className="searchForm" onSubmit={onSubmit} >
+            <motion.textarea className="searchForm__textarea"
+              ref={textSpace}
+              name=""
+              id=""
+              // cols="50"
+              // rows="20"
+              onChange={handleChange}
 
-        <div className="homeContainer">
-          <div className="searchAndResults" >
-            <form className="searchForm" onSubmit={onSubmit}>
-              <textarea className="searchForm__textarea"
-                ref={textSpace}
-                name=""
-                id=""
-                // cols="50"
-                // rows="20"
-                onChange={handleChange}
-              ></textarea>
-              <input className="searchForm__translateBtn" type="submit" value="translate" />
-            </form>
-            <div className="resultsContainer" >
-              {translations.map((item, index) => (
-                <p className="resultsContainer__result" key={index}>
-                  <strong>{item.lang}</strong>: {item.result}
-                </p>
-              ))}
-            </div>
-          </div>
-          <div className="searchedWordsContainer">
-            <h4 className="searchedWordsContainer__heading">previous searches...</h4>
-            {userHistory.map((item) => {
-              return (
-                <p className="searchedWordsContainer__word" key={item._id} onClick={handleTranslationClick}>
-                  {item.text}
-                </p>
-              );
-            })}
-          </div>
+              variants={inputLeftVariants}
+              animate="visible"
+              initial="hidden"></motion.textarea>
+            <motion.input className="searchForm__translateBtn" type="submit" value="translate" variants={slidDown}
+              animate="visible"
+              initial="hidden" />
+          </form>
+          <motion.div className="resultsContainer"
+            variants={inputRightVariants}
+            animate="visible"
+            initial="hidden"
+          >
+            {text.length > 0 && (isLoading ? "loading..." : (translations.map((item, index) => (
+              <p className="resultsContainer__result" key={index}>
+                <strong>{item.lang}</strong>: {item.result}
+              </p>
+            ))))}
+          </motion.div>
         </div>
-      </>)
-      }
-
+        <motion.div className="searchedWordsContainer" variants={slideUpVariants}
+          animate="visible"
+          initial="hidden">
+          <h4 className="searchedWordsContainer__heading">previous searches...</h4>
+          {userHistory.map((item) => {
+            return (
+              <p className="searchedWordsContainer__word" key={item._id} onClick={handleTranslationClick}>
+                {item.text}
+              </p>
+            );
+          })}
+        </motion.div>
+      </div>
     </>
   );
 };
